f8e7f98188139fd87769d338ef2012673f9ad2ad
avillasis22 <82188240+avillasis22@users.noreply.github.com>
2021-10-29 22:00:33 -0400

    Creative Writing Portfolio Website

---
+++ files/main.js
@@ -0,0 +1,119 @@
+sr.reveal('.mainImageTest, .caption, .smallCaption', {
+    origin: 'bottom',
+    distance: '30px',
+    duration: 800,
+    easing: 'ease',
+    mobile: true,
+    reset: true,
+    viewFactor: 0.2,
+});
+
+
+sr.reveal('.sectionHeader', {
+    origin: 'bottom',
+    distance: '20px',
+    duration: 800,
+    easing: 'ease',
+    mobile: true,
+    reset: true,
+    viewFactor: 0.4,
+});
+
+
+
+
+sr.reveal('.bioHeader', {
+    origin: 'bottom',
+    distance: '45px',
+    duration: 900,
+    easing: 'cubic-bezier(0.4, 0, 0.2, 1)',
+    mobile: true,
+    reset: true,
+    viewFactor: 0.23,
+
+});
+
+
+
+sr.reveal('.bioSubHeader', {
+    origin: 'bottom',
+    distance: '45px',
+    duration: 1000,
+    easing: 'cubic-bezier(0.4, 0, 0.2, 1)',
+    mobile: true,
+    reset: true,
+    viewFactor: 0.23,
+
+});
+
+
+
+sr.reveal('.bioText', {
+    origin: 'bottom',
+    distance: '45px',
+    duration: 1100,
+    easing: 'cubic-bezier(0.4, 0, 0.2, 1)',
+    mobile: true,
+    reset: true,
+    viewFactor: 0.23,
+});
+
+
+
+
+sr.reveal('.settingStoryTitleTest, .narrativeNonfictionTitleTest, .entryOne, .entryThree, .analyticaltitleText', {
+    origin: 'left',
+    distance: '45px',
+    duration: 1100,
+    easing: 'cubic-bezier(0.4, 0, 0.2, 1)',
+    mobile: true,
+    reset: true,
+    viewFactor: 0.23,
+
+});
+
+sr.reveal('.entryTwo', {
+    origin: 'right',
+    distance: '45px',
+    duration: 1100,
+    easing: 'cubic-bezier(0.4, 0, 0.2, 1)',
+    mobile: true,
+    reset: true,
+    viewFactor: 0.23,
+
+});
+
+
+sr.reveal('.gradientGalloDelCielo, .indent, .gradientNature, .gradientTextOne', {
+    origin: 'bottom',
+    distance: '45px',
+    duration: 1100,
+    easing: 'cubic-bezier(0.4, 0, 0.2, 1)',
+    mobile: true,
+    reset: true,
+    viewFactor: 0.01,
+});
+
+
+
+
+sr.reveal('.gradientEntryOne, .gradientEntryThree', {
+    origin: 'left',
+    distance: '45px',
+    duration: 1100,
+    easing: 'cubic-bezier(0.4, 0, 0.2, 1)',
+    mobile: true,
+    reset: true,
+    viewFactor: 0.01,
+});
+
+
+sr.reveal('.gradientEntryTwo', {
+    origin: 'right',
+    distance: '45px',
+    duration: 1100,
+    easing: 'cubic-bezier(0.4, 0, 0.2, 1)',
+    mobile: true,
+    reset: true,
+    viewFactor: 0.01,
+});
